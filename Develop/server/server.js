ed7c265260184210cf03e12b7b4661b18f4dacf0
Vagabond <marcusjjreyes@gmail.com>
2023-08-24 16:18:41 -0700

    worked on webpack config

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -10,3 +10,10 @@ app.use(express.json());
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+
+// 1. work on webpack config
+// 2. work on service worker
+// 3. set up indexedDB
+// 4. make app installable
+
